51aabadcb261ac846585a54717d96fe9b0b95abe
VIJAY YADAV <>
2024-06-08 01:02:05 +0530

    change content

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,228 +1,239 @@
 import { meta, shopify, starbucks, tesla } from "../assets/images";
 import {
-    car,
-    contact,
-    css,
-    estate,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    linkedin,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    pricewise,
-    react,
-    redux,
-    sass,
-    snapgram,
-    summiz,
-    tailwindcss,
-    threads,
-    typescript
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
 } from "../assets/icons";
 
 export const skills = [
-    {
-        imageUrl: css,
-        name: "CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: express,
-        name: "Express",
-        type: "Backend",
-    },
-    {
-        imageUrl: git,
-        name: "Git",
-        type: "Version Control",
-    },
-    {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
-    },
-    {
-        imageUrl: html,
-        name: "HTML",
-        type: "Frontend",
-    },
-    {
-        imageUrl: javascript,
-        name: "JavaScript",
-        type: "Frontend",
-    },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nextjs,
-        name: "Next.js",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
-    {
-        imageUrl: react,
-        name: "React",
-        type: "Frontend",
-    },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
-    },
-    {
-        imageUrl: sass,
-        name: "Sass",
-        type: "Frontend",
-    },
-    {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
-        type: "Frontend",
-    },
-    {
-        imageUrl: typescript,
-        name: "TypeScript",
-        type: "Frontend",
-    }
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
 ];
 
 export const experiences = [
-    {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
-        iconBg: "#accbe1",
-        date: "March 2020 - April 2021",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#fbc3bc",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#b7e4c7",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#a2d2ff",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
+  {
+    title: "Frontend Developer Intern with PHP",
+    company_name: "Websofy Software Private Limited",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "Jan 2023 - September 2023",
+    points: [
+      "During my internship at Websofy, I honed my skills in both client-side and server-side development, focusing on creating robust and user-friendly websites and web applications . This role provided a dynamic environment to expand my expertise across a broad spectrum of technologies including PHP, Wordpress,React.js, and Next.js.",
+      "I effectively combined PHP for server-side functionalities with React.js and Next.js for client-side development to deliver comprehensive web solutions.",
+      "I specialized in implementing responsive web designs utilizing CSS frameworks such as Bootstrap and Tailwind CSS, which optimized user experiences across different devices.",
+      "I deepened my technical skills in both frontend and backend development, gaining hands-on experience that bridged the gap between theoretical knowledge and practical application.",
+      "Under the guidance of experienced developers, I learned valuable project management techniques and teamwork strategies that are essential in a high-paced professional environment.",
+      "This internship was a pivotal step in my development career, providing me with the real-world experience necessary to excel in future roles in web development. At Websofy, I not only enhanced my technical skills but also developed a solid understanding of project delivery and team collaboration.",
+    ],
+  },
+  // {
+  //     title: "React Native Developer",
+  //     company_name: "Tesla",
+  //     icon: tesla,
+  //     iconBg: "#fbc3bc",
+  //     date: "Jan 2021 - Feb 2022",
+  //     points: [
+  //         "Developing and maintaining web applications using React.js and other related technologies.",
+  //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //         "Implementing responsive design and ensuring cross-browser compatibility.",
+  //         "Participating in code reviews and providing constructive feedback to other developers.",
+  //     ],
+  // },
+  // {
+  //     title: "Web Developer",
+  //     company_name: "Shopify",
+  //     icon: shopify,
+  //     iconBg: "#b7e4c7",
+  //     date: "Jan 2022 - Jan 2023",
+  //     points: [
+  //         "Developing and maintaining web applications using React.js and other related technologies.",
+  //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //         "Implementing responsive design and ensuring cross-browser compatibility.",
+  //         "Participating in code reviews and providing constructive feedback to other developers.",
+  //     ],
+  // },
+  {
+    title: "Full Stack & WordPress Developer",
+    company_name: "Websofy Software Private Limited",
+    icon: meta,
+    iconBg: "#a2d2ff",
+    date: "September 2023 - Present",
+    points: [
+      "As a Full Stack and WordPress Developer at Websofy, I leverage my expertise in WordPress and core PHP to build dynamic, high-performance websites. My role involves both front-end and back-end development, ensuring seamless, scalable, and robust website solutions.",
+      "Specialize in creating custom WordPress themes and plugins that enhance user engagement and improve SEO performance",
+      " Develop and maintain advanced full-stack applications using PHP, ensuring powerful server-side functionality coupled with responsive front-end design",
+      "Skilled in HTML, CSS, JavaScript, and PHP with a deep understanding of WordPress architecture and ecosystem.",
+      "At Websofy, my role not only involves coding but also strategizing and implementing the best practices in web development to deliver top-notch websites that drive business success.",
+    ],
+  },
 ];
 
 export const socialLinks = [
-    {
-        name: 'Contact',
-        iconUrl: contact,
-        link: '/contact',
-    },
-    {
-        name: 'GitHub',
-        iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
-    },
-    {
-        name: 'LinkedIn',
-        iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
-    }
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
 ];
 
 export const projects = [
-    {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-    }
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-red",
+    name: "Websofy Software Pvt. Ltd",
+    description:
+      "Developed a sleek, responsive website that combines modern aesthetics with robust functionality. Built with PHP, HTML5, CSS3, JavaScript, and Bootstrap 5.3, the site features a user-friendly interface and adaptive design, ensuring an optimal viewing experience across all devices and screen sizes.",
+    link: "https://www.websofy.com/",
+  },
+
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Days And Dates",
+    description:
+      "Discover a user-centric online resource for all your calendar needs at DaysAndDates.com. Our platform offers seamless navigation to effortlessly find important dates, historical events, and holiday schedules. Enhanced with intuitive tools and a responsive design, our website ensures you can plan and organize your time efficiently whether you’re on a desktop or on-the-go. Developed using modern technologies like HTML5, CSS3, JavaScript, and Bootstrap 5.3, DaysAndDates.com delivers a smooth and engaging user experience across all devices.",
+    link: "https://daysanddates.com/",
+  },
+
+  //   {
+  //     iconUrl: car,
+  //     theme: "btn-back-blue",
+  //     name: "Car Finding App",
+  //     description:
+  //       "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+  //     link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+  //   },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "shriramjanmabhumi",
+    description:
+      "Developed a sleek, responsive website that combines modern aesthetics with robust functionality. Built with PHP, HTML5, CSS3, JavaScript, and Bootstrap 5.3,Wordpress the site features a user-friendly interface and adaptive design, ensuring an optimal viewing experience across all devices and screen sizes.",
+    link: "https://www.shriramjanmabhumi.com/",
+  },
+  //   {
+  //     iconUrl: estate,
+  //     theme: "btn-back-black",
+  //     name: "Real-Estate Application",
+  //     description:
+  //       "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
+  //     link: "https://github.com/adrianhajdin/projects_realestate",
+  //   },
+  //   {
+  //     iconUrl: summiz,
+  //     theme: "btn-back-yellow",
+  //     name: "AI Summarizer Application",
+  //     description:
+  //       "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+  //     link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  //   },
 ];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,35 +10,35 @@ import "react-vertical-timeline-component/style.min.css";
 
 const About = () => {
   return (
-    <section className='max-container'>
-      <h1 className='head-text'>
+    <section className="max-container">
+      <h1 className="head-text">
         Hello, I'm{" "}
-        <span className='blue-gradient_text font-semibold drop-shadow'>
+        <span className="blue-gradient_text font-semibold drop-shadow">
           {" "}
           Vijay
         </span>{" "}
         👋
       </h1>
 
-      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
-          Software Engineer based in Croatia, specializing in technical
-          education through hands-on learning and building applications.
+          Software Engineer from India, specializing in technical education
+          through hands-on learning and building applications,building websites.
         </p>
       </div>
 
-      <div className='py-10 flex flex-col'>
-        <h3 className='subhead-text'>My Skills</h3>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
 
-        <div className='mt-16 flex flex-wrap gap-12'>
+        <div className="mt-16 flex flex-wrap gap-12">
           {skills.map((skill) => (
-            <div className='block-container w-20 h-20' key={skill.name}>
-              <div className='btn-back rounded-xl' />
-              <div className='btn-front rounded-xl flex justify-center items-center'>
+            <div className="block-container w-20 h-20" key={skill.name}>
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={skill.imageUrl}
                   alt={skill.name}
-                  className='w-1/2 h-1/2 object-contain'
+                  className="w-1/2 h-1/2 object-contain"
                 />
               </div>
             </div>
@@ -46,16 +46,16 @@ const About = () => {
         </div>
       </div>
 
-      <div className='py-16'>
-        <h3 className='subhead-text'>Work Experience.</h3>
-        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience.</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
           <p>
             I've worked with all sorts of companies, leveling up my skills and
             teaming up with smart people. Here's the rundown:
           </p>
         </div>
 
-        <div className='mt-12 flex'>
+        <div className="mt-12 flex">
           <VerticalTimeline>
             {experiences.map((experience, index) => (
               <VerticalTimelineElement
@@ -63,11 +63,11 @@ const About = () => {
                 date={experience.date}
                 iconStyle={{ background: experience.iconBg }}
                 icon={
-                  <div className='flex justify-center items-center w-full h-full'>
+                  <div className="flex justify-center items-center w-full h-full">
                     <img
                       src={experience.icon}
                       alt={experience.company_name}
-                      className='w-[60%] h-[60%] object-contain'
+                      className="w-[60%] h-[60%] object-contain"
                     />
                   </div>
                 }
@@ -79,22 +79,19 @@ const About = () => {
                 }}
               >
                 <div>
-                  <h3 className='text-black text-2xl font-poppins font-semibold'>
+                  <h3 className="text-black text-2xl font-poppins font-semibold">
                     {experience.title}
                   </h3>
-                  <p
-                    className='text-black font-bold '
-                    style={{ margin: 0 }}
-                  >
+                  <p className="text-black font-bold " style={{ margin: 0 }}>
                     {experience.company_name}
                   </p>
                 </div>
 
-                <ul className='my-5 list-disc ml-5 space-y-2'>
+                <ul className="my-5 list-disc ml-5 space-y-2">
                   {experience.points.map((point, index) => (
                     <li
                       key={`experience-point-${index}`}
-                      className='text-black font-normal pl-1 text-lg'
+                      className="text-black font-normal pl-1 text-lg"
                     >
                       {point}
                     </li>
@@ -106,7 +103,7 @@ const About = () => {
         </div>
       </div>
 
-      <hr className='border-black' />
+      <hr className="border-black" />
 
       <CTA />
     </section>
